37df324aa3d95fb2e015532aef3c8be01130b98b
Diyah <diyahm@aol.com>
2013-01-27 22:04:34 -0800

    update routes to redirect to eventbrite

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Hbcuhacks::Application.routes.draw do
   get "pages/index"
 
+  match "/atl" => redirect("http://startupsathbcus-atl.eventbrite.com/")
+  match "/dc" => redirect("http://startupsathbcus-dc.eventbrite.com/")
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
