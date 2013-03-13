ff1710426e291f59d31677c26a5f03c3ab02a784
Diyah <me@hadiyah.me>
2013-03-12 22:17:51 -0700

    Add event detail page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ActiveRecord::Base
-  attr_accessible :address, :desc, :end_date, :prizes, :start_date
+  attr_accessible :address, :desc, :end_date, :prizes, :start_date, :title
+  validates :title, presence: true
+  validates :address, presence: true
+  validates :start_date, presence: true
 
   scope :upcoming, lambda { where("start_date > ?", DateTime.now).order("start_date ASC").limit(3) }
 end
